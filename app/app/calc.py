ff81cf0f807331702e98aac5caae14581f630d63
Abeltemi <abeltemitayoade@gmail.com>
2024-11-20 03:38:56 +0100

    add github actions

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculation function
+"""
+
+def add(x, y):
+    """ This adds two values X and Y and returns the result."""
+    return x+y
+
+def subtract(x, y):
+    """This function returns subtraction between x and y."""
+    return x - y
---
+++ app/app/tests.py
@@ -0,0 +1,21 @@
+"""
+Simple tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module"""
+    
+    def test_add_numbers(self):
+        """Test adding numbers together"""
+        
+        res = calc.add(3, 5)
+        self.assertEqual(res, 8)
+    
+    def test_subtract_numbers(self):
+        
+        res = calc.subtract(4, 8)
+        self.assertEqual(res, 4)
