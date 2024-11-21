2ad86c87f568d0ab8ed256016db36d4a4f35658f
Abeltemi <abeltemitayoade@gmail.com>
2024-11-21 01:02:49 +0100

    error fixed

--- app/app/calc.py
+++ app/app/calc.py
@@ -6,6 +6,8 @@ def add(x, y):
     """ This adds two values X and Y and returns the result."""
     return x+y
 
+
 def subtract(x, y):
     """This function returns subtraction between x and y."""
     return x - y
+
